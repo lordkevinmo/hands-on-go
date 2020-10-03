9eb288ca6e457a6203dd4631b7f9b451709faeca
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-10-03 11:42:04 +0200

    hand-on middleware

---
+++ src/chapter3/customMiddleware/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func middleware(originalHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Executing middleware before request phase!")
+		originalHandler.ServeHTTP(w, r)
+		fmt.Println("Executing middleware after request phase!")
+	})
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	// Business logic goes here
+	fmt.Println("Executing mainHandler")
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	originalHandler := http.HandlerFunc(handle)
+	http.Handle("/", middleware(originalHandler))
+	http.ListenAndServe(":8000", nil)
+}
---
+++ src/chapter3/multipleMiddleware/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type city struct {
+	Name string
+	Area uint64
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var tempCity city
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&tempCity)
+		if err != nil {
+			panic(err)
+		}
+		defer r.Body.Close()
+		fmt.Printf("Got %s city with area %d sq miles! \n", tempCity.Name, tempCity.Area)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("201 - Created"))
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("405 - Method not allowed"))
+	}
+}
+
+func main() {
+	originalHandler := http.HandlerFunc(postHandler)
+	http.Handle("/city", filterContentType(setServerTimeCookie(originalHandler)))
+	http.ListenAndServe(":8000", nil)
+}
+
+func filterContentType(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Currently In the check content type middleware")
+		if r.Header.Get("Content-type") != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte("415 - Unsupported Media Type. Please send JSON"))
+			return
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func setServerTimeCookie(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+		// Setting cookie to every API response
+		cookie := http.Cookie{Name: "Server-Time(UTC", Value: strconv.FormatInt(time.Now().Unix(), 10)}
+		http.SetCookie(w, &cookie)
+		log.Println("Currently in the set server time middleware")
+	})
+}
