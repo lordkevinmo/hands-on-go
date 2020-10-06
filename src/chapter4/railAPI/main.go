6b474503aacf958f0f422d086a98d6db7a3e2625
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-10-06 12:29:34 +0200

    add station endpoint

--- src/chapter4/railAPI/main.go
+++ src/chapter4/railAPI/main.go
@@ -38,10 +38,10 @@ type ScheduleResource struct {
 	ArrivalTime time.Time
 }
 
-// Register adds paths and routes to a new service instance
+// Register adds paths and routes to a new service instance for train
 func (t *TrainResource) Register(container *restful.Container) {
 	ws := new(restful.WebService)
-	ws.Path("/v1/trains").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+	ws.Path("/v1/train").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
 
 	ws.Route(ws.GET("/{train-id}").To(t.getTrain))
 	ws.Route(ws.POST("").To(t.createTrain))
@@ -50,26 +50,64 @@ func (t *TrainResource) Register(container *restful.Container) {
 	container.Add(ws)
 }
 
-func main() {
-	var err error
-	// Connect to database
-	DB, err = sql.Open("sqlite3", "./railAPI.db")
+// Register adds paths and routes to a new service instance for station
+func (s *StationResource) Register(container *restful.Container) {
+	ws := new(restful.WebService)
+	ws.Path("/v1/station").Consumes(restful.MIME_JSON).Produces(restful.MIME_JSON)
+
+	ws.Route(ws.GET("/{station-id}").To(s.getStation))
+	ws.Route(ws.POST("").To(s.createStation))
+	ws.Route(ws.DELETE("/{station-id}").To(s.removeStation))
+
+	container.Add(ws)
+}
+
+// GET http://localhost:8000/v1/station/1
+func (s StationResource) getStation(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("station-id")
+	err := DB.QueryRow("SELECT ID, NAME, OPENING_TIME, CLOSING_TIME FROM station where id=?", id).Scan(&s.ID, &s.Name, &s.OpeningTime, &s.ClosingTime)
 	if err != nil {
-		log.Println("Driver Creation failed!")
+		log.Println(err)
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusNotFound, "Station could not be found")
+	} else {
+		response.WriteEntity(s)
 	}
-	// Create tables
-	dbutils.Initialize(DB)
-	// Instantiate container
-	wsContainer := restful.NewContainer()
-	wsContainer.Router(restful.CurlyRouter{})
-	t := TrainResource{}
-	t.Register(wsContainer)
-	log.Printf("Start listening on localhost:8000")
-	server := &http.Server{
-		Addr:    ":8000",
-		Handler: wsContainer,
+}
+
+// POST http://localhost:8000/v1/station
+func (s StationResource) createStation(request *restful.Request, response *restful.Response) {
+	log.Println(request.Request.Body)
+	decoder := json.NewDecoder(request.Request.Body)
+	var tempStation StationResource
+	err := decoder.Decode(&tempStation)
+	log.Println(tempStation.Name)
+	if err != nil {
+		log.Println(err)
 	}
-	log.Fatal(server.ListenAndServe())
+	statement, _ := DB.Prepare("INSERT INTO station (NAME, OPENING_TIME, CLOSING_TIME) values (?, ?, ?)")
+	result, err := statement.Exec(tempStation.Name, tempStation.OpeningTime, tempStation.ClosingTime)
+	if err == nil {
+		newID, _ := result.LastInsertId()
+		tempStation.ID = int(newID)
+		response.WriteHeaderAndEntity(http.StatusCreated, tempStation)
+	} else {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
+}
+
+// DELETE http://localhost:8000/station/1
+func (s StationResource) removeStation(request *restful.Request, response *restful.Response) {
+	id := request.PathParameter("station-id")
+	statement, _ := DB.Prepare("DELETE FROM station where id=?")
+	_, err := statement.Exec(id)
+	if err == nil {
+		response.WriteHeader(http.StatusOK)
+	} else {
+		response.AddHeader("Content-Type", "application/json")
+		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+	}
 }
 
 // GET http://localhost:8000/v1/trains/1
@@ -118,4 +156,32 @@ func (t TrainResource) removeTrain(request *restful.Request, response *restful.R
 		response.AddHeader("Content-Type", "text/plain")
 		response.WriteErrorString(http.StatusInternalServerError, err.Error())
 	}
+}
+
+func main() {
+	log.Println("Time", time.Now())
+	var err error
+	// Connect to database
+	DB, err = sql.Open("sqlite3", "./railAPI.db")
+	if err != nil {
+		log.Println("Driver Creation failed!")
+	}
+	// Create tables
+	dbutils.Initialize(DB)
+	// Instantiate container
+	wsContainer := restful.NewContainer()
+	wsContainer.Router(restful.CurlyRouter{})
+	// train endpoint processing
+	t := TrainResource{}
+	t.Register(wsContainer)
+	// station endpoint processing
+	s := StationResource{}
+	s.Register(wsContainer)
+
+	log.Printf("Start listening on localhost:8000")
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: wsContainer,
+	}
+	log.Fatal(server.ListenAndServe())
 }
